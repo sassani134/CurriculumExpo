29325366a22dc564ba344e810d9086015b777d6a
Samuel <sassani134@gmail.com>
2021-04-20 08:52:05 +0200

    first push

--- App.js
+++ App.js
@@ -1,11 +1,39 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React,{ useState, useEffect } from 'react';
+import { StyleSheet, Text, View, FlatList, ActivityIndicator, Button} from 'react-native';
 
 export default function App() {
+
+  const [isLoading, setLoading ] = useState(true);
+  const [data, setData] =useState({});
+
+  useEffect(() => {
+    console.log("Data has changed", data);
+  },[data]);
+  
+
+  useEffect(() =>{
+    fetch('https://curriculum-rails.herokuapp.com/home/index.json')
+      .then((response) => response.json())
+      .then((json) => setData(json))
+      .catch((error) => console.error(error))
+      .finally(()=> setLoading(false));
+  }, []);
+
+
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      {isLoading ? 
+      
+      <ActivityIndicator/>
+      :
+        <Button 
+        onPress={() => console.log("test",data, "fin")}
+        title="test"
+        />
+      }
       <StatusBar style="auto" />
     </View>
   );
